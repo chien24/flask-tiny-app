c51379870c9a8570090e8da4245ce9abab063682
user-season <hoaxuanhoan@gmail.com>
2025-03-03 21:44:41 +0700

    Reapply "merge conflict"
    This reverts commit a3ca255360d27b7f8f6549303f5fd6ed085f6822.

--- blog_web/blog_web/settings.py
+++ blog_web/blog_web/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blogApp.apps.BlogappConfig',
-    'accounts.apps.AccountsConfig',
 
 ]
 
@@ -128,6 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# chuyển hướng người dùng tới trang đang nhập khi chưa đang nhập
-LOGIN_URL = '/accounts/signin'
