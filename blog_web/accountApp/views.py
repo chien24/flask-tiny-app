67d5a8da561c9a59ba18800ecfa8dce21016f50c
user-season <hoaxuanhoan@gmail.com>
2025-03-05 12:36:12 +0700

    completed version 2

--- blog_web/accountApp/views.py
+++ blog_web/accountApp/views.py
@@ -45,19 +45,12 @@ def login_view(request):
             messages.error(request, 'Username and Password are required.')
             return render(request, 'account/login.html')
         
-        user = UserBlog.objects.get(username=username)
+        user = authenticate(username=username, password=password)
         
         if user is not None:
-            if user.is_active is False:
-                messages.success(request, 'Your account was banned! Contact admin now.')
-                return render(request, 'account/login.html')
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                messages.success(request, 'Login successfully!')
-                return redirect('blogApp:home')
-            messages.error(request, 'Username or Password is incorrect.')
-            return render(request, 'account/login.html')
+            login(request, user)
+            messages.success(request, 'Login successfully!')
+            return redirect('blogApp:home')
         else:
             messages.error(request, 'Username or Password is incorrect.')
             return render(request, 'account/login.html')
